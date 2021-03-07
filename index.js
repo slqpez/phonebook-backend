4ab0577412759c830147b63967ba5abc2e2e9fc4
slqpez <santiago.lopez16@udea.edu.co>
2021-03-06 20:56:45 -0500

    Phonebook backend step7.

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require("express")
 const app = express()
 const Router = require("./routes/person.routes.js")
 require('dotenv').config()
+const morgan = require('morgan')
+
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms'
+    ].join(' ')
+  }))
 
 app.use(express.json())
 const port = process.env.PORT 
