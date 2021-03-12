514377b5904f15263a8ff73bbfe9643e70ac8d21
slqpez <santiago.lopez16@udea.edu.co>
2021-03-12 16:54:58 -0500

    Step3 fiexd from front id.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
 const Router = require("./routes/person.routes.js")
+const cors = require('cors')
 require('dotenv').config()
 const morgan = require('morgan')
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(cors())
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 
 
